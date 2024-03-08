d2f0e1976842150dcabd76c22a9080be1d7f80fe
DESKTOP-2A5H47P\Jamal <jamalshahidi20@gmail.com>
2024-03-08 20:53:15 +0330

    Fix Api

--- Test/Test.Api/Controllers/UserController.cs
+++ Test/Test.Api/Controllers/UserController.cs
@@ -1,6 +1,47 @@
-﻿namespace Test.Api.Controllers
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Test.Application.Responses;
+
+namespace Test.Api.Controllers
 {
-    public class UserController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        
+        public UserController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+
+        [HttpGet]
+        public async Task<Response> GetUsers([FromQuery] int? id, [FromQuery] int from = 0, [FromQuery] int to = 10)
+        {
+            return new Response();
+        }
+
+
+        [HttpPost]
+        public async Task<Response> AddUser()
+        {
+            return new Response();
+        }
+
+        [HttpPut]
+        public async Task<Response> UpdateUser()
+        {
+            return new Response();
+        }
+
+        [HttpDelete]
+        public async Task<Response> DeleteUser()
+        {
+            return new Response();
+        }
+
+
+
     }
 }
--- Test/Test.Api/Program.cs
+++ Test/Test.Api/Program.cs
@@ -1,15 +1,20 @@
+using Test.Application;
+using Test.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = builder.Configuration;
+
+
+builder.Services.ConfigureApplicationServices()
+                .ConfigurePersistenceServices(configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
