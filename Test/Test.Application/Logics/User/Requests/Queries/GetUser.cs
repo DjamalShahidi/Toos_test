45a24f3ff107836a5ffc52abf0145f421ef36bf4
DESKTOP-2A5H47P\Jamal <jamalshahidi20@gmail.com>
2024-03-08 13:18:47 +0330

    Foldering

---
+++ Test/Test.Application/Logics/User/Requests/Queries/GetUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Application.Logics.User.Requests.Queries
+{
+    internal class GetUser
+    {
+    }
+}
---
+++ Test/Test.Application/Responses/Response.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Application.Responses
+{
+    internal class Response
+    {
+    }
+}
