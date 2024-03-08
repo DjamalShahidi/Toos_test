cdf28d40f053da26ac3c4c569c32a1e6ee2b22bf
DESKTOP-2A5H47P\Jamal <jamalshahidi20@gmail.com>
2024-03-08 21:00:47 +0330

    Fix Some mapping

--- Test/Test.Application/Profiles/MappingProfile.cs
+++ Test/Test.Application/Profiles/MappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Test.Domain;
 
 namespace Test.Application.Profiles
 {
@@ -8,6 +9,28 @@ namespace Test.Application.Profiles
         {
             //CreateMap<PreInvoiceHeader, AddPreInvoiceHeaderDto>().ReverseMap();
 
+        }
+
+        private UserDetailValueType MapValueType(object value)
+        {
+            var stValue = value.ToString();
+
+            if (int.TryParse(stValue, out _))
+            {
+                return UserDetailValueType.Int;
+            }
+            else if (bool.TryParse(stValue, out _))
+            {
+                return UserDetailValueType.Boolean;
+            }
+            //else if (date.TryParse(stValue, out _)) DateTime
+            //{
+            //    return UserDetailValueType.Boolean;
+            //}
+            else
+            {
+                return UserDetailValueType.String;
+            }
         }
 
     }
